071f6884f2a5c3090ce9a23d412d5c26d0970771
topstepengineer <118773249+topstepengineer@users.noreply.github.com>
2022-11-22 10:07:16 +0100

    Add files via upload

--- xlsxTOcsv.py
+++ xlsxTOcsv.py
@@ -7,13 +7,13 @@ read_file = pd.read_excel ("F255_04_VEH_KPIs.xlsx")
   
 # Write the dataframe object
 # into csv file
-read_file.to_csv ("F255_04_VEH_KPIs.csv", 
+read_file.to_csv ("myData.csv", 
                   index = None,
                   header=True)
     
 # read csv file and convert 
 # into a dataframe object
-df = pd.DataFrame(pd.read_csv("F255_04_VEH_KPIs.csv"))
+df = pd.DataFrame(pd.read_csv("myData.csv"))
   
 # show the dataframe
 df
