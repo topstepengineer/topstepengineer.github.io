d6f38a8b185341d119af5584f45ca8c6ea249bb0
topstepengineer <118773249+topstepengineer@users.noreply.github.com>
2022-12-06 15:17:54 +0100

    Update Engine.js

--- files/settings/Engine.js
+++ files/settings/Engine.js
@@ -3,18 +3,6 @@
 // ------------------------------------------------
 i=0;
 
-// i = i + 1;
-// dia_title		[i] = ["Left Tire Gas Pressures"];	// define the titles
-// channels		[i] = ["P_FL_mean"];	// define the channels for the diagrams
-// min_max			[i] = [20, 32];	// define the min/max for the diagram y-axis
-// unit			[i] = ["psi"];	// define the units
-// color_channels	[i] = [colDefault];	// colors for channels ("" = standard color)
-// pos_axis		[i] = [0];	// define position for y-axis (0 = left, 1 = right)
-// dash_channels	[i] = [0];	// dash for channels (0 = no dash)
-// trend_line		[i] = [];	// define values for trend lines
-// trendline_color	[i] = [""];	// Define colors for trend lines
-// precision		[i] = [];	// define number of decimal places for y-axis
-
 i = i + 1;
 dia_title		[i] = ["Min/Max RPM"];	// define the titles
 channels		[i] = ["FCT_nEngineOnThrottle_Max", "FCT_nEngineOnThrottle_Min"];	// define the channels for the diagrams
@@ -38,6 +26,19 @@ dash_channels	[i] = [0, 0];	// dash for channels (0 = no dash)
 trend_line		[i] = [];	// define values for trend lines
 trendline_color	[i] = [""];	// Define colors for trend lines
 precision		[i] = [];	// define number of decimal places for y-axis
+
+i = i + 1;
+dia_title		[i] = ["PPU Illegal Integral"];	// define the titles
+channels		[i] = ["FIA_PPUIllegal_Max"];	// define the channels for the diagrams
+min_max			[i] = [0, 60];	// define the min/max for the diagram y-axis
+unit			[i] = [" Too much power! (kW*sec)"];	// define the units
+color_channels	[i] = [colDefault];	// colors for channels ("" = standard color)
+pos_axis		[i] = [""];	// define position for y-axis (0 = left, 1 = right)
+dash_channels	[i] = [0];	// dash for channels (0 = no dash)
+trend_line		[i] = [];	// define values for trend lines
+trendline_color	[i] = [""];	// Define colors for trend lines
+precision		[i] = [];	// define number of decimal places for y-axis
+
 
 i = i + 1;
 dia_title		[i] = ["TC Switches"];	// define the titles
