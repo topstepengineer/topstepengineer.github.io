1ba1325f770d47171c0ee439a28fe085c6d9f3e3
topstepengineer <118773249+topstepengineer@users.noreply.github.com>
2023-01-24 11:16:26 -0500

    Update set_options.js

--- files/settings/general/set_options.js
+++ files/settings/general/set_options.js
@@ -14,9 +14,9 @@ var maxLaptime = 113;
 
 var maxSpeed = 305;
 
-var Drv1 = 'FUO' //'c68e366a-5a8f-4f3a-bd81-84b1e9762a1b';
-var Drv2 = 'APG' //'36151730-b89c-4305-84e7-445b05646872';
-var Drv3 = 'CAL' //'de7faf50-914b-4b92-b263-15aeabb7dc9c';
+var Drv1 = 'APG' //'c68e366a-5a8f-4f3a-bd81-84b1e9762a1b';
+var Drv2 = 'CAL' //'36151730-b89c-4305-84e7-445b05646872';
+var Drv3 = 'GIO' //'de7faf50-914b-4b92-b263-15aeabb7dc9c';
 
 var timeZoneOffset = 4;
 
